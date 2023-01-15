2fb22fe5f1e4b69195917d964ee7cdfad627b591
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-15 13:51:36 -0800

    Adds test api call to my-blog page with fetch from api/hello file

--- pages/blogs/my-blog.tsx
+++ pages/blogs/my-blog.tsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react"
+
 export default function Blog() {
+  useEffect(() => {
+    fetch('/api/hello')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      
+    })
+  }, [])
+
   return <h1>Nested routing test</h1>
 }
